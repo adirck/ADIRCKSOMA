7fcd8ce350f5a0fd5bb7fc968f59c1dda3d61b8d
adirck <alexandre.dirck@gmail.com>
2026-03-02 12:21:04 -0300

    Update parametro TEF

---
+++ UpdateTef.sql
@@ -0,0 +1,3 @@
+update PARAMETROS_LOJA
+set VALOR_ATUAL = 'C:\DPOS8\bin\Qt_Files_ClientLinxTEF_8.22.23.0012_x32-Windows\dposconfig.exe'
+where PARAMETRO = 'TEF_CLIENT_PATH' and CODIGO_FILIAL = 'CDF'
